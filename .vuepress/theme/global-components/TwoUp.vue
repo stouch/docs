e6b0bd17a3fb1dd7f7af0d049ca637f862555d3f
rijkvanzanten <rijkvanzanten@me.com>
2020-01-09 18:00:54 -0500

    Start restructuring docs

---
+++ .vuepress/theme/global-components/TwoUp.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="two-up">
+		<div class="left">
+			<slot name="left" />
+		</div>
+
+		<div class="right">
+			<slot name="right" />
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.two-up {
+	margin: 12px 0;
+	display: grid;
+	grid-gap: 3rem;
+	grid-template-columns: 1fr;
+
+	> div {
+		min-width: 0;
+	}
+
+	@media (min-width: 1158px) {
+		grid-template-columns: 4fr 3fr;
+	}
+}
+
+</style>
